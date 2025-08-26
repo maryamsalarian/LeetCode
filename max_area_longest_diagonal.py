6b50141f3de67028671f6257356e96ee38a75ffc
maryamsalarian <89162393+maryamsalarian@users.noreply.github.com>
2025-08-26 19:28:01 -0400

    improved max_area

--- max_area_longest_diagonal.py
+++ max_area_longest_diagonal.py
@@ -1,20 +1,34 @@
-# initial solution:
-
-
 class Solution(object):
     def areaOfMaxDiagonal(self, dimensions):
         """
         :type dimensions: List[List[int]]
         :rtype: int
         """
-        prev_d = 0
-        prev_area = 0
-        for rectangle in dimensions:
-            d = (rectangle[0] ** 2) + (rectangle[1] ** 2)
-            area = rectangle[0] * rectangle[1]
-            if d > prev_d:
-                prev_d = d
-                prev_area = area
-            elif d == prev_d and area > prev_area:
-                prev_area = area
-        return prev_area
+
+        # initial solution:
+
+        # prev_d = 0
+        # prev_area = 0
+        # for rectangle in dimensions:
+        #     d = (rectangle[0] ** 2) + (rectangle[1] ** 2)
+        #     area = rectangle[0] * rectangle[1]
+        #     if d > prev_d:
+        #         prev_d = d
+        #         prev_area = area
+        #     elif d == prev_d and area > prev_area:
+        #         prev_area = area
+        # return prev_area
+
+        # improved solution:
+
+        max_d = 0
+        max_area = 0
+        for h, w in dimensions:
+            area = h * w
+            d = h * h + w * w
+            if d > max_d:
+                max_d = d
+                max_area = area
+            if d == max_d:
+                max_area = max(max_area, area)
+        return max_area
