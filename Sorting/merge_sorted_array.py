48a46eaec006290c28092d18dbd253e8864a7502
maryamsalarian <89162393+maryamsalarian@users.noreply.github.com>
2025-09-05 17:36:19 -0400

    python sort function

--- Sorting/merge_sorted_array.py
+++ Sorting/merge_sorted_array.py
@@ -47,3 +47,12 @@ class Solution(object):
 
         # return nums1
 
+
+        # # python sort function:
+
+        # for i in range(n):
+        #     nums1[m+i] = nums2[i]
+        # nums1.sort()
+
+        # return nums1
+
