c668120dec7e73e35bba2ea97285c5f98d90df72
maryamsalarian <89162393+maryamsalarian@users.noreply.github.com>
2025-09-05 17:34:44 -0400

    two pointer, worse memory solution

--- Sorting/merge_sorted_array.py
+++ Sorting/merge_sorted_array.py
@@ -29,3 +29,21 @@ class Solution(object):
 
         return nums1
 
+
+        # # two pointer 2:
+
+        # p1 = m-1
+        # p2 = n-1
+        # pm = n+m-1
+
+        # while p2 >= 0:
+        #     if p1 >= 0 and nums1[p1] > nums2[p2]:
+        #         nums1[pm] = nums1[p1]
+        #         p1 -= 1
+        #     else:
+        #         nums1[pm] = nums2[p2]
+        #         p2 -= 1
+        #     pm -= 1
+
+        # return nums1
+
