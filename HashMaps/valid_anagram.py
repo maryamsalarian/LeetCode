3037b9c1f19968c86aa08a32be5ba10e1901e0b7
maryamsalarian <89162393+maryamsalarian@users.noreply.github.com>
2025-09-09 21:13:12 -0400

    valid anagram, 2 hashmaps

--- HashMaps/valid_anagram.py
+++ HashMaps/valid_anagram.py
@@ -9,24 +9,51 @@ class Solution(object):
         :rtype: bool
         """
         # intuitive hashmap, 1 hashmap
+        
+        # s_chars = defaultdict(lambda: 1)
+        # for char in s:
+        #     if not char in s_chars:
+        #         s_chars[char]
+        #     else:
+        #         s_chars[char] += 1
+        # # by the end, s_chars includes all chars and their count for str s
+        # for char in t:
+        #     if char in s_chars:
+        #         s_chars[char] -= 1
+        #         if s_chars[char] < 0:
+        #             return False
+        #         else:
+        #             continue
+        #     else:
+        #         return False
+        # # if by then, there are any key:value pair left with value > 0, means that all chars were not matchec > not anagram
+        # for char in s_chars:
+        #     if s_chars[char] != 0:
+        #         return False
+        # # return True
+
+        # intuitive hashmap, 2 hashmaps
+
+        if len(s) != len(t):  # ensure lengths are equal
+            return False
+
         s_chars = defaultdict(lambda: 1)
+        t_chars = defaultdict(lambda: 1)
+        # for str s
         for char in s:
             if not char in s_chars:
                 s_chars[char]
             else:
                 s_chars[char] += 1
-        # by the end, s_chars includes all chars and their count for str s
+        # for str t
         for char in t:
-            if char in s_chars:
-                s_chars[char] -= 1
-                if s_chars[char] < 0:
-                    return False
-                else:
-                    continue
+            if not char in t_chars:
+                t_chars[char]
             else:
+                t_chars[char] += 1
+        for key in s_chars:
+            if not key in t_chars:  # ensure key exists in both maps
                 return False
-        # if by then, there are any key:value pair left with value > 0, means that all chars were not matchec > not anagram
-        for char in s_chars:
-            if s_chars[char] != 0:
+            if s_chars[key] != t_chars[key]:
                 return False
-        # return True
+        return True
