5abe22bbdca34c32892525d571c4afd47cec54ca
maryamsalarian <89162393+maryamsalarian@users.noreply.github.com>
2025-09-16 20:11:19 -0400

    array intersection, using set

--- Sorting/find_array_intersection.py
+++ Sorting/find_array_intersection.py
@@ -6,12 +6,22 @@ class Solution(object):
         :rtype: List[int]
         """
         # method 1: using nested for loop. time: O(n^2). memroy: O(n)
-        result = []
-        for num1 in nums1:
-            for num2 in nums2:
-                if num1 == num2:
-                    result.append(num1)
-        no_dupes = set(result)
-        return list(no_dupes)
-        return set(result)
+        # result = []
+        # for num1 in nums1:
+        #     for num2 in nums2:
+        #         if num1 == num2:
+        #             result.append(num1)
+        # no_dupes = set(result)
+        # return list(no_dupes)
+        # return set(result)
+
+        # # method 2: using set intersection
+        set1 = set(nums1)
+        set2 = set(nums2)
+        res_set = set1.intersection(set2)
+        res = []
+        for element in res_set:
+            res.append(element)
+
+        return res
 
