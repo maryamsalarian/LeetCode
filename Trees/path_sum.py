fb163aef69b448be42b81a1ddc2be973d5f93731
maryamsalarian <maryamsalariann@gmail.com>
2026-03-03 23:25:52 -0500

    iterative stack

--- Trees/path_sum.py
+++ Trees/path_sum.py
@@ -13,7 +13,7 @@ class Solution(object):
         :type targetSum: int
         :rtype: bool
         """
-        # recusrive solution - poor on memory:
+        # # recusrive solution - poor on memory:
         # if not root:
         #     return False
         # if not root.left and not root.right:
@@ -21,16 +21,30 @@ class Solution(object):
         # remaining = targetSum - root.val
         # return (self.hasPathSum(root.left, remaining) or self.hasPathSum(root.right, remaining))
 
-        # iterative - deque:
+        # # iterative - deque:
+        # if not root:
+        #     return False
+        # my_list = deque([(root, targetSum - root.val)])
+        # while my_list:
+        #     node, remaining = my_list.popleft()
+        #     if not node.left and not node.right and remaining == 0:
+        #         return True
+        #     if node.let:
+        #         my_list.append((node.left, remaining - node.left.val))
+        #     if node.right:
+        #         my_list.append((node.right, remaining - node.right.val))
+
+        # iterative - stack:
         if not root:
             return False
-        my_list = deque([(root, targetSum - root.val)])
-        while my_list:
-            node, remaining = my_list.popleft()
+        my_s = [(root, targetSum - root.val)]
+        while my_s:
+            node, remaining = my_s.pop()
             if not node.left and not node.right and remaining == 0:
                 return True
-            if node.let:
-                my_list.append((node.left, remaining - node.left.val))
+            if node.left:
+                my_s.append((node.left, remaining - node.left.val))
             if node.right:
-                my_list.append((node.right, remaining - node.right.val))
+                my_s.append((node.right, remaining - node.right.val))
+        return False
 
